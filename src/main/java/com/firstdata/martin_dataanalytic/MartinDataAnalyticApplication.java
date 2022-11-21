6cabc3adb3e12be4f9814dbab4e16d8a0f53dc63
sangdo <sang.do@knorex.com>
2022-11-21 15:41:44 +0700

    - init project

---
+++ src/main/java/com/firstdata/martin_dataanalytic/MartinDataAnalyticApplication.java
@@ -0,0 +1,13 @@
+package com.firstdata.martin_dataanalytic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartinDataAnalyticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartinDataAnalyticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstdata/martin_dataanalytic/MartinDataAnalyticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstdata.martin_dataanalytic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartinDataAnalyticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
