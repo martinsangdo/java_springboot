a60ef843bd083848d77655817f6f4de85c48e0e1
sangdo <sang.do@knorex.com>
2024-02-21 00:37:04 +0700

    - scrape speaking

--- src/main/resources/python/dol.py
+++ src/main/resources/python/dol.py
@@ -6,27 +6,19 @@ from pymongo import MongoClient
 def query_page(page_index):
     # post_url = "https://apigateway.dolenglish.vn/public/search-transform/api/filter?contentGroup=DICTATION"
     # post_url = 'https://apigateway.dolenglish.vn/public/search-transform/api/filter'  #reading practice
-    post_url = 'https://apigateway.dolenglish.vn/public/search-transform/api/filter/samples'  #writing practice
+    post_url = 'https://apigateway.dolenglish.vn/public/search-transform/api/filter/samples'  #writing & speaking practice
 
     json_data = {
         "query": "",
         "filters": {
             "all": [
                 {
-                    "content_group": "WRITING"
-                },
-                {
-                    "type": "WRITING_TASK_2_ACADEMIC"
+                    "type": "SPEAKING_PART_3"
                 }
             ]
         },
         "facets": {
             "topic": [
-                {
-                    "type": "value"
-                }
-            ],
-            "chart_type": [
                 {
                     "type": "value"
                 }
@@ -56,18 +48,13 @@ def query_page(page_index):
     # print(list)
     return list
 #-------
-client = MongoClient('localhost:27017')
-db_client = client['martin_projects']['ielts_writing_task_2']
+# client = MongoClient('localhost:27017')
+# db_client = client['martin_projects']['ielts_writing_task_2']
 
 ids = []
-for page_index in range(1, 37):
+for page_index in range(1, 6):
     list = query_page(page_index)
     for item in list:
         link = 'https://tuhocielts.dolenglish.vn/' + item['url']['raw']
-        record = db_client.find_one({'link':link, 'is_done':{'$ne': True}})
-        if record is None:
-            #not existed
-            print(link)
-        # else:
-        #     print('existed: ' + link)
+        print(link)
 
