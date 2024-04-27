9619bd0719f8ced9697c8f7eed9c3d171211b23f
sangdo <sang.do@knorex.com>
2024-04-27 15:54:32 +0700

    - add global exception (not working)

---
+++ src/main/java/com/firstdata/martin_dataanalytic/RestExceptionHandler.java
@@ -0,0 +1,81 @@
+package com.firstdata.martin_dataanalytic;
+
+import lombok.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//https://www.toptal.com/java/spring-boot-rest-api-error-handling
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public static final String TRACE = "trace";
+
+//    @Value("${reflectoring.trace:false}")
+//    private boolean printStackTrace;
+
+    @Override
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request
+    ) {
+        //Body omitted as it's similar to the method of same name
+        // in ProductController example...
+        //.....
+        System.out.println(ex); //todo: not working
+        return null;
+    }
+/*
+    @ExceptionHandler(ItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleItemNotFoundException(
+            ItemNotFoundException itemNotFoundException,
+            WebRequest request
+    ){
+        //Body omitted as it's similar to the method of same name
+        // in ProductController example...
+        //.....
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleAllUncaughtException(
+            RuntimeException exception,
+            WebRequest request
+    ){
+        //Body omitted as it's similar to the method of same name
+        // in ProductController example...
+        //.....
+    }
+
+    //....
+
+    @Override
+    public ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+
+        return buildErrorResponse(ex,status,request);
+    }
+*/
+}
