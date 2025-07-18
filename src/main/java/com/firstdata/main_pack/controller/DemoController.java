8fd0dda97d8b793925194657405ee9e325b6a491
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-07-18 16:15:14 +0700

    - create sample POST APIs

--- src/main/java/com/firstdata/main_pack/controller/DemoController.java
+++ src/main/java/com/firstdata/main_pack/controller/DemoController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +20,35 @@ public class DemoController {
         productNames.add("Samsung");
 
         return new ResponseEntity<>(productNames, HttpStatus.OK);
+    }
+
+    @GetMapping("/product/detail")
+    public ResponseEntity getProductById(
+        @RequestParam String id
+    ){
+        System.out.println("Id value: " + id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @PostMapping("/product/detail")
+    public ResponseEntity createNewProduct(
+        @RequestParam String name,
+        @RequestParam(required = false) String price,
+        @RequestParam(defaultValue = "yellow") String color
+    ){
+        System.out.println("Name value: " + name);
+        System.out.println("Price value: " + price);
+        System.out.println("Color value: " + color);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/product/detail/body")
+    public ResponseEntity createNewProductFromBodyParam(
+        @RequestBody Object productDetail
+    ){
+        System.out.println("Body data: " + productDetail.toString());
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
